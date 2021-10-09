239d76874ca8da9e486417b6fc5b1f17eee88b5f
Oscar Hernandez <os.hf024@gmail.com>
2021-10-08 20:12:33 -0500

    feat: Fetch all caracters and cache them

--- src/controllers/characters.controllers.js
+++ src/controllers/characters.controllers.js
@@ -1,7 +1,41 @@
 import axios from 'axios';
+import redis from "redis";
+import { promisify } from 'util';
+
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+const REDIS_GET = promisify(client.get).bind(client);
+const REDIS_SET = promisify(client.set).bind(client);
+
+const cacheResponse = async ({ url, key }) => {
+
+  const cachedResponse = await REDIS_GET(key);
+
+  if (cachedResponse) {
+    return JSON.parse(cachedResponse);
+  }
+
+  const response = await axios.get(url);
+  const { data } = response;
+
+  await REDIS_SET(key, JSON.stringify(data));
+
+  return data;
+
+}
 
 export const characters = async (req, res) => {
-  res.json({ message: 'Hello World!' });
+
+  const data = await cacheResponse({
+    url: 'https://rickandmortyapi.com/api/character',
+    key: 'characters'
+  });
+
+  res.json(data);
+
 };
 
 export const character = (req, res) => {
